de1c1c6b1fabfddf8d80fd01996841a1ec58ee7b
KhangBenTree <nguyenphucbinhkhang@gmail.com>
2022-04-30 09:55:45 +0700

    update database

--- Database/build_database.sql
+++ Database/build_database.sql
@@ -18,107 +18,85 @@ CREATE SCHEMA IF NOT EXISTS `cse job fair registration` DEFAULT CHARACTER SET ut
 USE `cse job fair registration` ;
 
 -- -----------------------------------------------------
--- Table `cse job fair registration`.`Account`
+-- Table `cse job fair registration`.`admin`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cse job fair registration`.`Account` (
-  `idAccount` VARCHAR(20) NOT NULL,
-  `Account_email` VARCHAR(45) NOT NULL,
-  `Account_pass` VARCHAR(45) NOT NULL,
-  `Account_sdt` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idAccount`),
-  UNIQUE INDEX `idAccount_UNIQUE` (`idAccount` ASC) VISIBLE)
+CREATE TABLE IF NOT EXISTS `cse job fair registration`.`admin` (
+  `idAdmin` VARCHAR(10) NOT NULL,
+  `Name` NVARCHAR(45) NOT NULL,
+  `Mail` VARCHAR(45) NOT NULL,
+  `Pass` VARCHAR(45) NOT NULL,
+  `Phone` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idAdmin`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `cse job fair registration`.`Admin`
+-- Table `cse job fair registration`.`booth`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cse job fair registration`.`Admin` (
-  `idAdmin` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`idAdmin`),
-  CONSTRAINT `fk_admin_account1`
-    FOREIGN KEY (`idAdmin`)
-    REFERENCES `cse job fair registration`.`Account` (`idAccount`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+CREATE TABLE IF NOT EXISTS `cse job fair registration`.`booth` (
+  `idBooth` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`idBooth`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `cse job fair registration`.`DoanhNghiep`
+-- Table `cse job fair registration`.`bussiness`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cse job fair registration`.`DoanhNghiep` (
-  `idDN` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`idDN`),
-  CONSTRAINT `fk_doanhnghiep_account`
-    FOREIGN KEY (`idDN`)
-    REFERENCES `cse job fair registration`.`Account` (`idAccount`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+CREATE TABLE IF NOT EXISTS `cse job fair registration`.`bussiness` (
+  `Name` VARCHAR(45) NOT NULL,
+  `Mail` VARCHAR(45) NOT NULL,
+  `Pass` VARCHAR(45) NOT NULL,
+  `Phone` VARCHAR(45) NOT NULL,
+  `idBussiness` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`idBussiness`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `cse job fair registration`.`GianHang`
+-- Table `cse job fair registration`.`registration`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cse job fair registration`.`GianHang` (
-  `idGianHang` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`idGianHang`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `cse job fair registration`.`DoanhNghiep_DK_GianHang`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cse job fair registration`.`DoanhNghiep_DK_GianHang` (
-  `DoanhNghiep_idDN` VARCHAR(20) NOT NULL,
-  `GianHang_idGianHang` VARCHAR(20) NOT NULL,
-  `ThoiGian` DATETIME NOT NULL,
-  `idDangKy` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idDangKy`, `DoanhNghiep_idDN`, `GianHang_idGianHang`),
-  INDEX `fk_DoanhNghiep_DK_GianHang_GianHang1_idx` (`GianHang_idGianHang` ASC) VISIBLE,
-  INDEX `fk_DoanhNghiep_DK_GianHang_DoanhNghiep1_idx` (`DoanhNghiep_idDN` ASC) VISIBLE,
-  CONSTRAINT `fk_DoanhNghiep_DK_GianHang_GianHang1`
-    FOREIGN KEY (`GianHang_idGianHang`)
-    REFERENCES `cse job fair registration`.`GianHang` (`idGianHang`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_DoanhNghiep_DK_GianHang_DoanhNghiep1`
-    FOREIGN KEY (`DoanhNghiep_idDN`)
-    REFERENCES `cse job fair registration`.`DoanhNghiep` (`idDN`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+CREATE TABLE IF NOT EXISTS `cse job fair registration`.`registration` (
+  `idRegistration` VARCHAR(10) NOT NULL,
+  `Bussiness_idBussiness` VARCHAR(10) NOT NULL,
+  `Booth_idBooth` VARCHAR(10) NOT NULL,
+  `Time_registration` DATETIME NOT NULL,
+  PRIMARY KEY (`idRegistration`),
+  INDEX `fk_Registration_Booth_idx` (`Booth_idBooth` ASC) VISIBLE,
+  INDEX `fk_Registration_Bussiness1_idx` (`Bussiness_idBussiness` ASC) VISIBLE,
+  CONSTRAINT `fk_Registration_Booth`
+    FOREIGN KEY (`Booth_idBooth`)
+    REFERENCES `cse job fair registration`.`booth` (`idBooth`),
+  CONSTRAINT `fk_Registration_Bussiness1`
+    FOREIGN KEY (`Bussiness_idBussiness`)
+    REFERENCES `cse job fair registration`.`bussiness` (`idBussiness`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `cse job fair registration`.`Admin_Duyet_Hoac_Tuchoi_DK`
+-- Table `cse job fair registration`.`approve`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cse job fair registration`.`Admin_Duyet_Hoac_Tuchoi_DK` (
-  `Admin_idAdmin` VARCHAR(20) NOT NULL,
-  `Duyet` TINYINT(1) NULL,
-  `idDangKy` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idDangKy`),
-  INDEX `fk_Admin_Duyet_Hoac_Tuchoi_DK_DoanhNghiep_DK_GianHang1_idx` (`idDangKy` ASC) VISIBLE,
-  INDEX `fk_Admin_Duyet_Hoac_Tuchoi_DK_Admin1_idx` (`Admin_idAdmin` ASC) VISIBLE,
-  CONSTRAINT `fk_Admin_Duyet_Hoac_Tuchoi_DK_DoanhNghiep_DK_GianHang1`
-    FOREIGN KEY (`idDangKy`)
-    REFERENCES `cse job fair registration`.`DoanhNghiep_DK_GianHang` (`idDangKy`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Admin_Duyet_Hoac_Tuchoi_DK_Admin1`
+CREATE TABLE IF NOT EXISTS `cse job fair registration`.`approve` (
+  `Registration_idRegistration` VARCHAR(10) NOT NULL,
+  `Admin_idAdmin` VARCHAR(10) NOT NULL,
+  `Approve_or_Deny` TINYINT NULL DEFAULT NULL,
+  `Time_approve` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`Registration_idRegistration`, `Admin_idAdmin`),
+  INDEX `fk_Registration_has_Admin_Admin1_idx` (`Admin_idAdmin` ASC) VISIBLE,
+  INDEX `fk_Registration_has_Admin_Registration1_idx` (`Registration_idRegistration` ASC) VISIBLE,
+  CONSTRAINT `fk_Registration_has_Admin_Admin1`
     FOREIGN KEY (`Admin_idAdmin`)
-    REFERENCES `cse job fair registration`.`Admin` (`idAdmin`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    REFERENCES `cse job fair registration`.`admin` (`idAdmin`),
+  CONSTRAINT `fk_Registration_has_Admin_Registration1`
+    FOREIGN KEY (`Registration_idRegistration`)
+    REFERENCES `cse job fair registration`.`registration` (`idRegistration`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
